06b058f329c2772f976b262fa331a00c5a1ded7c
danilovxp <danilovxp@gmail.com>
2020-10-26 17:59:47 -0700

    Updates

---
+++ precache-manifest.c815744762b54189bfc217f662239623.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a892ed9ca2b252f3c0b93c0200002df2",
+    "url": "/CalculatorForKids/index.html"
+  },
+  {
+    "revision": "3ef53fd32c28259ae97c",
+    "url": "/CalculatorForKids/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8a81084fe390838882e0",
+    "url": "/CalculatorForKids/static/css/main.3ee105f3.chunk.css"
+  },
+  {
+    "revision": "3ef53fd32c28259ae97c",
+    "url": "/CalculatorForKids/static/js/2.13ecbbfb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CalculatorForKids/static/js/2.13ecbbfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a81084fe390838882e0",
+    "url": "/CalculatorForKids/static/js/main.d5e26a6b.chunk.js"
+  },
+  {
+    "revision": "2b64f19e88b90186deea",
+    "url": "/CalculatorForKids/static/js/runtime-main.029745bb.js"
+  },
+  {
+    "revision": "a0d81982e00e6882f978ba0525f7c771",
+    "url": "/CalculatorForKids/static/media/bg.a0d81982.jpg"
+  }
+]);
